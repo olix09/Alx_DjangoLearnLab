67a114a87d835464955b17bed8bb48e485ed0f66
OLIX <151432001+olix09@users.noreply.github.com>
2025-07-16 12:17:53 +0300

    Add files via upload

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -31,10 +31,26 @@ class Library(models.Model):
     def __str__(self):
         return self.name
 
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField(blank=True, null=True)
+    birth_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Librarian(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    library = models.ForeignKey(Library, on_delete=models.CASCADE)  # Each librarian is assigned to a library
+
+    def __str__(self):
+        return f"{self.user.username} - {self.library.name}"
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
     published_date = models.DateField()
+    library = models.ForeignKey(Library, on_delete=models.CASCADE)  # Each book belongs to a library
 
     def __str__(self):
         return self.title
