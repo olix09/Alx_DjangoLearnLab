7f3a31eec1f78944923d8e09e99db16da360bf58
OLIX <151432001+olix09@users.noreply.github.com>
2025-07-27 17:01:07 +0300

    Delete advanced_features_and_security directory

--- advanced_features_and_security/bookshelf/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import CustomUser, Book
-from django.contrib.auth.admin import UserAdmin
-
-admin.site.register(CustomUser, UserAdmin)
-admin.site.register(Book)
