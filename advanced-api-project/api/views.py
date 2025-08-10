91e67262aef5065a3abd4d8f998ff8b494b4f7ea
OLIX <151432001+olix09@users.noreply.github.com>
2025-08-10 18:49:04 +0300

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,10 +1,12 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters import rest_framework  # <-- add this exact import
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 
 from .models import Book
 from .serializers import BookSerializer
+
 
 class BookListView(generics.ListAPIView):
     """
