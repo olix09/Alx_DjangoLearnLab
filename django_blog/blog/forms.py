6563313126206a1bbea84e0a620d5c6b9b46352d
OLIX <151432001+olix09@users.noreply.github.com>
2025-08-16 18:34:57 +0300

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,7 +1,21 @@
 from django import forms
-from .models import Post
+from .models import Post, Comment
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
