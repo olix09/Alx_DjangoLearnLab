b1a906f4c9887c159b0cd0224dc7ee43b5f989c7
OLIX <151432001+olix09@users.noreply.github.com>
2025-08-24 20:07:33 +0300

    Update urls.py

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-    path('', include('posts.urls')),  # <-- added
+    path('api/accounts/', include('accounts.urls')),  # changed to include "api/"
+    path('api/', include('posts.urls')),  # changed to include "api/"
 ]
