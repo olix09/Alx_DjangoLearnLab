68752ec9457345d29c4b7e6e6bf1a910bbfed056
OLIX <151432001+olix09@users.noreply.github.com>
2025-08-24 20:09:38 +0300

    Update models.py

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class User(AbstractUser):
     bio = models.TextField(blank=True, null=True)
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    # existing field from step 1 (keep as-is)
     followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
-
-    def __str__(self):
-        return self.username
+    # NEW: explicit following field as required in this step
+    following = models.ManyToManyField('self', symmetrical=False, related_name='followers_rel', blank=True)
