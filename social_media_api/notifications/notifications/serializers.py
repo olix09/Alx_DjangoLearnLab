2e0714fd9d93057ec5122a665545ad12ae49ea8f
OLIX <151432001+olix09@users.noreply.github.com>
2025-08-24 20:25:38 +0300

    Delete social_media_api/notifications/notifications directory

--- social_media_api/notifications/notifications/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Notification
-
-class NotificationSerializer(serializers.ModelSerializer):
-    actor = serializers.ReadOnlyField(source='actor.username')
-    
-    class Meta:
-        model = Notification
-        fields = ['id', 'actor', 'verb', 'target_object_id', 'timestamp', 'is_read']
